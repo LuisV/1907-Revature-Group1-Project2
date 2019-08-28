47b323ba06c441fd063c64520cd0efe151f304af
_hans <kiessler.hans@gmail.com>
2019-08-28 12:16:52 -0400

    Incomplete skeleton code for Hibernate DAOs

---
+++ HeroArena/src/main/java/com/revature/data/hibernate/UserHibernate.java
@@ -0,0 +1,23 @@
+package com.revature.data.hibernate;
+
+import com.revature.beans.User;
+import com.revature.data.UserDAO;
+
+public class UserHibernate implements UserDAO {
+
+	public int addUser(User user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public User getUser(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User banUser(User u) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
