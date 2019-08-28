cbef438c80bf46ee7d72364fea2383926400b6c4
_hans <kiessler.hans@gmail.com>
2019-08-28 12:50:45 -0400

    Imported utility classes and working on User and UserHibernate

--- HeroArena/src/main/java/com/revature/data/hibernate/UserHibernate.java
+++ HeroArena/src/main/java/com/revature/data/hibernate/UserHibernate.java
@@ -1,13 +1,33 @@
 package com.revature.data.hibernate;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import com.revature.beans.User;
 import com.revature.data.UserDAO;
+import com.revature.utils.HibernateUtil;
+import com.revature.utils.LogUtil;
+
 
 public class UserHibernate implements UserDAO {
-
+	private HibernateUtil hu = HibernateUtil.getInstance();
+	
 	public int addUser(User user) {
-		// TODO Auto-generated method stub
-		return 0;
+		Session s = hu.getSession();
+		Transaction t = null;
+		Integer i = 0;
+		try {
+			t = s.beginTransaction();
+			i = (Integer) s.save(user);
+			t.commit();
+		} catch (HibernateException e) {
+			t.rollback();
+			LogUtil.logException(e, UserHibernate.class);
+		} finally {
+			s.close();
+		}
+		return i;
 	}
 
 	public User getUser(String username, String password) {
