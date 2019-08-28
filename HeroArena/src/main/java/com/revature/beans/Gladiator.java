e340064b707e84821f25d9737d37fccfdcc11453
_hans <kiessler.hans@gmail.com>
2019-08-28 15:20:27 -0400

    Fixed Gladiator +1

--- HeroArena/src/main/java/com/revature/beans/Gladiator.java
+++ HeroArena/src/main/java/com/revature/beans/Gladiator.java
@@ -1,30 +1,45 @@
 package com.revature.beans;
 
 public class Gladiator {
+	private String name;
 	private Integer strength;
 	private Integer dexterity;
 	private Integer vitality;
 	private Integer experience;
+	private Integer level;
 	private Integer currentHealth;
 	private Integer maxHealth;
 	
 	public Gladiator() {
 		super();
+		this.name = "no-arg";
 		this.strength = 2;
 		this.dexterity = 2;
 		this.vitality = 0;
-		this.maxHealth = 5;
 		this.experience = 0;
+		this.level = 1;
+		this.maxHealth = 5;
+		this.currentHealth = this.maxHealth;
 	}
 	
 	public Gladiator(Integer strength, Integer dexterity, Integer vitality) {
 		super();
+		this.name = "str/dex/vit";
 		this.strength = 2 + strength;
 		this.dexterity = 2 + dexterity;
 		this.vitality = vitality;
 		this.maxHealth = 5 + (2*vitality);
+		this.currentHealth = this.maxHealth;
 		this.experience = 0;
 		
+	}
+
+	public Gladiator(String name, Integer strength, Integer dexterity, Integer vitality) {
+		super();
+		this.name = name;
+		this.strength = strength;
+		this.dexterity = dexterity;
+		this.vitality = vitality;
 	}
 
 	public Integer getStrength() {
