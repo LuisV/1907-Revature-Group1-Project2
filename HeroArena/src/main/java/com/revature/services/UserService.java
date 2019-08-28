c7d017750fbc26748eef1d397811e20239642c78
_hans <kiessler.hans@gmail.com>
2019-08-28 14:36:10 -0400

    Need to refactor yet again to fully incorporate Spring MVC

--- HeroArena/src/main/java/com/revature/services/UserService.java
+++ HeroArena/src/main/java/com/revature/services/UserService.java
@@ -1,4 +1,16 @@
 package com.revature.services;
 
-public interface UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.beans.User;
+import com.revature.data.UserDAO;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserDAO ud;
+	public User login(String username, String password) {
+		return ud.getUser(username, password);
+	}
 }
