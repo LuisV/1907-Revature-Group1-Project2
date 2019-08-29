2d65ef63b74fcc13013b30c356c5c845b46e3b0f
Luis Valencia <lvalencia@csumb.edu>
2019-08-28 20:33:10 -0400

    Created Gladiator tables and entries

--- HeroArena/src/main/resources/DB_Setup_02-DDL.sql
+++ HeroArena/src/main/resources/DB_Setup_02-DDL.sql
@@ -13,6 +13,17 @@ CREATE TABLE users (
     role NUMBER(1) DEFAULT 1 NOT NULL   -- For now,  0 == admin, 1 == player
 );
 
+CREATE TABLE gladiator (
+    id number(20) PRIMARY KEY,
+    name varchar2(36),
+    strength number(4),
+    dexterity number(4),
+    vitality number(4),
+    max_health number(4),
+    rarity number(5,2)
+);
+
 
 -- Create sequences to generate primary keys
 CREATE SEQUENCE USER_SEQ INCREMENT BY 1 START WITH 3;  -- Manually inserting 2 entries to start.
+CREATE SEQUENCE GLADIATOR_SEQ INCREMENT BY 1 START WITH 3;  -- Manually inserting 2 entries to start.
--- HeroArena/src/main/resources/DB_Setup_03-DML.sql
+++ HeroArena/src/main/resources/DB_Setup_03-DML.sql
@@ -5,4 +5,8 @@ INSERT INTO users (id, username, password, role)
 INSERT INTO users (id, username, password, role)
     VALUES (2, 'player', 'p4ssw0rd', 1);
     
+INSERT INTO gladiator (id, name, strength, dexterity, vitality, max_health, rarity) VALUES (1, 'King Cobra',  5, 3, 0, 15, .3 );
+INSERT INTO gladiator (id, name, strength, dexterity, vitality, max_health, rarity) VALUES (1, 'Shadow Lord',  3, 5, 0, 12, .3 );
+
+    
 COMMIT;
