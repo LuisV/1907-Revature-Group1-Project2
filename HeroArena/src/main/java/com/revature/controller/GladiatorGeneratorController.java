afe8e866f77cf8855c08ba36a7126354cbcbf7f7
_hans <kiessler.hans@gmail.com>
2019-08-30 13:39:54 -0400

    Started to create a GladiatorGenerator service

---
+++ HeroArena/src/main/java/com/revature/controller/GladiatorGeneratorController.java
@@ -0,0 +1,29 @@
+package com.revature.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.revature.beans.Gladiator;
+import com.revature.beans.GladiatorGenerator;
+
+@Controller
+@CrossOrigin
+@RequestMapping(value="/gladiator/generate")
+public class GladiatorGeneratorController {
+
+	private GladiatorGenerator gg = GladiatorGenerator.getGladiatorGenerator();
+	
+	@ResponseBody
+	@RequestMapping(method=RequestMethod.GET)
+	public Gladiator generateGladiator(HttpSession session) {
+		Integer star = 4;
+		return GladiatorGenerator.generateGladiator("Testgladiator", star);
+	}
+	
+	
+}
---
+++ HeroArena/src/main/java/com/revature/services/GladiatorService.java
@@ -0,0 +1,13 @@
+package com.revature.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.data.GladiatorDAO;
+
+@Service
+public class GladiatorService {
+	@Autowired
+	private GladiatorDAO gd;
+	
+}
