50811425709a280ebe3f8e34afa9bb0cc9462eea
Jim Yount <jimayount@gmail.com>
2019-09-03 07:51:20 -0400

    Login with Main Page

--- HeroArena-App/src/app/app.module.ts
+++ HeroArena-App/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
