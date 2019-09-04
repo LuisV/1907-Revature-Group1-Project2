8228a842c0ca3e39b9c1bd0376fc88ea84316adf
_hans <kiessler.hans@gmail.com>
2019-09-04 15:16:53 -0400

    can now get gladiators with the URL

--- HeroArena-App/src/app/roster.service.ts
+++ HeroArena-App/src/app/roster.service.ts
@@ -2,22 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Gladiator } from './gladiator';
 import { longStackSupport } from 'q';
+import { AuthenticateService } from './authenticate.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 // Need to be able to take in a value at '2'
-const baseUrl = 'http://localhost:8080/HeroArena/user/roster/2';
+const baseUrl = 'http://localhost:8080/HeroArena/user/roster/';
 
-let userId = 0;
+// let userId = 0;
 
 export class RosterService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthenticateService) { }
 
   // gladiators: Array<Gladiator> = [];
   gladiators: Array<Gladiator> = [];
+
+  userId = this.auth.getUser()['id'];
+  // console.log(userId);
+
 
   createGladiator(o): Gladiator {
     let g = new Gladiator();
@@ -25,19 +30,22 @@ export class RosterService {
     // TODO
     return g;
   }
+
   getGladiators() {
     // const header = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
-    const msg = this.http.get(baseUrl /* + userId*/, {withCredentials: true});
-    console.log(msg);
-    console.log('userId' + (Object.keys(msg)[0])); // [0][1]['player']['id']);
-
+    console.log(baseUrl + this.userId);
+    const msg = this.http.get(baseUrl + this.userId, {withCredentials: true});
+    // console.log('msg: ' + msg);
+    // console.log('userId' + (Object.keys(msg)[0])); // [0][1]['player']['id']);
+    // console.log('userId: ');
+    // console.log(this.userId);
     msg.forEach(element => {
       console.log(element);
       let g = new Gladiator();
       element = Object.entries(element);
       console.log(element);
-      userId = element[0][1]['player']['id'];
-      console.log(userId);
+      // userId = element[0][1]['player']['id'];
+      console.log(this.userId);
       // console.log(element[0][1]);
       for (let e in element) {
         console.log(element[e][1]);
