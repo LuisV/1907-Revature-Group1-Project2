1ac2ab3b86b2e1aa90fa73be1e67316e86d71f32
Jim Yount <jimayount@gmail.com>
2019-09-03 21:34:22 -0400

    Robust Registration Front-End

--- HeroArena-App/src/app/login/login.component.ts
+++ HeroArena-App/src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { AuthenticateService } from '../authenticate.service';
 import { User } from '../user';
 
 var registering = false;
-var passMatch = true;
+var passError = 0;
 
 @Component({
   selector: 'app-login',
@@ -45,25 +45,50 @@ export class LoginComponent implements OnInit {
   }
 
   register() {
+
     const username = (<HTMLInputElement>document.getElementById('username')).value;
     const password = (<HTMLInputElement>document.getElementById('password1')).value;
     const password2 = (<HTMLInputElement>document.getElementById('password2')).value;
 
-    if (password == password2) {
-      passMatch = true;
+    if (password == password2 && password.length >= 6 && username.length >= 3) {
+      passError = 0;
       const registerString = `username=${username}&password=${password}`;
       console.log(registerString);
 
       this.authent.addPlayer(registerString).subscribe((userObj: Object) => {
         this.authent.setUser(userObj);
+        registering = false;
       })
     }
-    else {
-      passMatch = false;
+    
+    console.log((passError & 1) + ' ' + (passError & 2) + ' ' + (passError & 4));
+    if (password != password2){
+      passError = passError | 1;
     }
+    else{
+      passError = (passError & 2) + (passError & 4);
+    }
+    console.log(passError);
+
+    if (password.length < 6){
+      passError = passError | 2;
+    }
+    else{
+      passError = (passError & 1) + (passError & 4);
+    }
+    console.log(passError);
+
+    if (username.length < 3){
+      passError = passError | 4;
+    }
+    else{
+      passError = (passError & 1) + (passError & 2);
+    }
+
+    console.log(passError);
   }
 
-  getPassMatch(){
-    return passMatch;
+  getPassError(flag){
+    return passError & flag;
   }
 }
