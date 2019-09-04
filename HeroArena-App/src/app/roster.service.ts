86d0f3120203dca461674681b8fc00fbc38f4210
_hans <kiessler.hans@gmail.com>
2019-09-04 15:04:24 -0400

    Can't get the userId without the get request, can't make the proper get request without the userId

--- HeroArena-App/src/app/roster.service.ts
+++ HeroArena-App/src/app/roster.service.ts
@@ -1,36 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Gladiator } from './gladiator';
+import { longStackSupport } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 
 // Need to be able to take in a value at '2'
-const url = 'http://localhost:8080/HeroArena/user/roster/2';
+const baseUrl = 'http://localhost:8080/HeroArena/user/roster/2';
 
+let userId = 0;
 
 export class RosterService {
 
   constructor(private http: HttpClient) { }
 
   // gladiators: Array<Gladiator> = [];
-  gladiators = [];
+  gladiators: Array<Gladiator> = [];
 
+  createGladiator(o): Gladiator {
+    let g = new Gladiator();
+    g.name = o['name'];
+    // TODO
+    return g;
+  }
   getGladiators() {
     // const header = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
-    const msg = this.http.get(url, {withCredentials: true});
+    const msg = this.http.get(baseUrl /* + userId*/, {withCredentials: true});
     console.log(msg);
+    console.log('userId' + (Object.keys(msg)[0])); // [0][1]['player']['id']);
+
     msg.forEach(element => {
       console.log(element);
       let g = new Gladiator();
-
-      // g.name = element['name'];
-      // tslint:disable-next-line: forin
-
+      element = Object.entries(element);
       console.log(element);
+      userId = element[0][1]['player']['id'];
+      console.log(userId);
+      // console.log(element[0][1]);
+      for (let e in element) {
+        console.log(element[e][1]);
+        g = this.createGladiator(element[e][1]);
+        this.gladiators.push(g);
+      }
+      console.log(this.gladiators);
 
-      this.gladiators.push(element);
+      // this works to get the userId
+      //console.log(element[0][1]['player']['id']);
     });
   }
 }
