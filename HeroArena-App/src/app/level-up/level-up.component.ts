6b7a25bfec3d60b3dce0dab6e72850e19f89a669
_hans <kiessler.hans@gmail.com>
2019-09-06 19:25:12 -0400

    Previous update broke the whole thing

--- HeroArena-App/src/app/level-up/level-up.component.ts
+++ HeroArena-App/src/app/level-up/level-up.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { RosterDisplayComponent } from '../roster-display/roster-display.component';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import {ChangeDetectorRef} from '@angular.core';
+//import {ChangeDetectorRef} from '@angular.core';
 import { Gladiator } from '../gladiator';
 import { RosterService } from '../roster.service';
 
