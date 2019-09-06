6d6c000279142c4cf003a5e57b3f6f30fad8ee2c
Jim Yount <jimayount@gmail.com>
2019-09-06 07:50:14 -0400

    Basic Battle Movement

--- HeroArena-App/src/app/battle/battle.component.ts
+++ HeroArena-App/src/app/battle/battle.component.ts
@@ -12,14 +12,17 @@ export class BattleComponent implements OnInit {
 
   //@ViewChild('canvasId', {static: false}) myCanvas: ElementRef;
   private context: CanvasRenderingContext2D;
+  private playerContext: CanvasRenderingContext2D;
 
   constructor(private pss: PagestateService) {
   }
 
-  private x = 0;
-  private y = 0;
   private width = 1100;
   private height = 600;
+  private playerx = -550;
+  private playery = 0;
+  private playerimagex = 0;
+  private playerimagey = -520;
 
   makeCanvas(canvas: HTMLCanvasElement) {
     this.context = canvas.getContext('2d');
@@ -28,37 +31,53 @@ export class BattleComponent implements OnInit {
 
   draw() {
     // do stuff
-    console.log("showing battle window")
+    //console.log("showing battle window")
 
-    let image = new Image();
-    image.src = 'assets/RROD.jpg';
-    this.context.fillStyle = "green";
+    this.context.fillStyle = "ffffff";
     this.context.fillRect(0, 0, this.width, this.height);
-    this.context.drawImage(image, this.x, this.y);
 
     if (this.pss.getState() == 3)
       window.requestAnimationFrame(() => this.draw());
   }
 
+  makeCanvasPlayer(canvas: HTMLCanvasElement) {
+    this.playerContext = canvas.getContext('2d');
+    window.requestAnimationFrame(() => this.drawPlayer());
+  }
+
+  drawPlayer() {
+    let image = new Image();
+    image.src = 'assets/shadowlord.png';
+    this.playerContext.drawImage(image, this.playerimagex, this.playerimagey);
+    (<HTMLCanvasElement>document.getElementById('player')).style.top = this.playery + "px";
+    (<HTMLCanvasElement>document.getElementById('player')).style.left = this.playerx + "px";
+
+
+    if (this.pss.getState() == 3)
+      window.requestAnimationFrame(() => this.drawPlayer());
+  }
+
   ngOnInit() {
     const canvas = <HTMLCanvasElement>document.getElementById('canvasId');
     this.makeCanvas(canvas);
+    const playerCanvas = <HTMLCanvasElement>document.getElementById('player');
+    this.makeCanvasPlayer(playerCanvas);
   }
 
   onKeydown(event) {
     //console.log(event);
 
     if (event.key === "ArrowUp") {
-      this.y -= 20;
+      this.playery -= 20;
     }
     if (event.key === "ArrowDown") {
-      this.y += 20;
+      this.playery += 20;
     }
     if (event.key === "ArrowLeft") {
-      this.x -= 20;
+      this.playerx -= 20;
     }
     if (event.key === "ArrowRight") {
-      this.x += 20;
+      this.playerx += 20;
     }
   }
 }
