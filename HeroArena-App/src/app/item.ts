3ee4a8257072a22371d562b4e7d13f020716766c
Luis Valencia <lvalencia@csumb.edu>
2019-09-08 15:16:57 -0400

    i dunno

---
+++ HeroArena-App/src/app/item.ts
@@ -0,0 +1,8 @@
+export class Item {
+  id: number;
+  name: string;
+  description: string;
+  healthOffset: number;
+  xpOffset: number;
+  price: number;
+}
---
+++ HeroArena-App/src/app/shop-list/shop-list.component.ts
@@ -0,0 +1,22 @@
+import { Item } from './../item';
+import { Component, OnInit } from '@angular/core';
+import { InventoryShopService } from '../inventory-shop.service';
+
+@Component({
+  selector: 'app-shop-list',
+  templateUrl: './shop-list.component.html',
+  styleUrls: ['./shop-list.component.css']
+})
+export class ShopListComponent implements OnInit {
+
+  constructor( private is: InventoryShopService) { }
+  shop: any;
+
+  ngOnInit() {
+    this.is.getShopItems().subscribe((obj:Object) => {
+      this.shop = obj;
+      console.log( this.shop );
+    });
+  }
+
+}
