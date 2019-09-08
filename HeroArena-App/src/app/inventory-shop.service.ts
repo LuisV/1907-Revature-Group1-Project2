3c8f90f916bc9c7d76a2f3399c6fd5475fcacb19
Luis Valencia <lvalencia@csumb.edu>
2019-09-08 16:42:25 -0400

    added shop interface, did Dan's front-end work

--- HeroArena-App/src/app/inventory-shop.service.ts
+++ HeroArena-App/src/app/inventory-shop.service.ts
@@ -1,18 +1,16 @@
+import { AuthenticateService } from './authenticate.service';
 import { Item } from './item';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Inventory } from './inventory';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { User } from './user';
 
 const url = 'http://localhost:8080/HeroArena/';
 
 export class InventoryShopService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private as: AuthenticateService) { }
 
   getShopItems() {
     const headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
@@ -20,6 +18,6 @@ export class InventoryShopService {
   }
   purchaseItemFromShop(itemNum: number) {
     const headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
-    return this.http.put( url + 'shop/items/' + itemNum, {headers, withCredentials: true}).pipe(map(resp => resp as Inventory));
+    return this.http.put( url + 'shop/items/' + itemNum +"?userId=" + this.as.getUser().id, {headers, withCredentials: true}).pipe(map(resp => resp as Inventory));
   }
 }
