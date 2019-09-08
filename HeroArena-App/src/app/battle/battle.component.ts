4c05bd049137944a79aa73a972467bf73980f618
Jim Yount <jimayount@gmail.com>
2019-09-08 12:35:52 -0400

    Progress on Attaching Battle

--- HeroArena-App/src/app/battle/battle.component.ts
+++ HeroArena-App/src/app/battle/battle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { PagestateService } from '../pagestate.service';
+import { BattleService } from '../battle.service';
 
 
 @Component({
@@ -17,7 +18,7 @@ export class BattleComponent implements OnInit {
   private playerHealthContext: CanvasRenderingContext2D;
   private enemyHealthContext: CanvasRenderingContext2D;
 
-  constructor(private pss: PagestateService) {
+  constructor(private pss: PagestateService, private bs: BattleService) {
   }
 
   private width = 1100;
@@ -122,6 +123,15 @@ export class BattleComponent implements OnInit {
 
     this.playerimagex[1] = 0;
     this.playerimagey[1] = -650;
+
+    
+    //this.bs.getPlayerGladiator().subscribe((userObj: Object) => {
+
+    //});
+
+    //this.bs.getOpponentGladiator().subscribe((userObj: Object) => {
+      
+    //});
 
     (<HTMLCanvasElement>document.getElementById('canvasId')).focus();
 
@@ -294,7 +304,8 @@ export class BattleComponent implements OnInit {
     }
 
     if (event.key === " ") {
-      this.damagePlayer(<HTMLCanvasElement>document.getElementById('player'), (<HTMLCanvasElement>document.getElementById('player')).getContext('2d'), 0, 1);
+      //if (!this.playerDamaging[0])
+        this.damagePlayer(<HTMLCanvasElement>document.getElementById('player'), (<HTMLCanvasElement>document.getElementById('player')).getContext('2d'), 0, 1);
     }
 
     //console.log(this.keys);
