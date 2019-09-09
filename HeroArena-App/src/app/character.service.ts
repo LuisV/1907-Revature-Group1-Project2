2784c087f1d4ced3b172ca9d500ed9f6eeafdd33
Jim Yount <jimayount@gmail.com>
2019-09-09 08:54:46 -0400

    Merging

--- HeroArena-App/src/app/character.service.ts
+++ HeroArena-App/src/app/character.service.ts
@@ -11,10 +11,14 @@ import { AuthenticateService } from './authenticate.service';
 //})
 
 // Need to be able to take in a value at '2'
-// const url = 'http://localhost:8080/HeroArena/user/roster/2';
-const url = 'http://localhost:8080/HeroArena/';
+const url = 'http://18.221.9.229/HeroArena/';
+//const url = 'http://localhost:8080/HeroArena/';
 
-const baseUrl = 'http://localhost:8080/HeroArena/user/roster/';
+const baseUrl = 'http://18.221.9.229/HeroArena/user/roster/';
+//const baseUrl = 'http://localhost:8080/HeroArena/user/roster/';
+
+
+// const url = 'http://localhost:8080/HeroArena/user/roster/2';
 // const baseUrl = 'http://localhost:8080/HeroArena/user/roster/2';
 
 export class CharacterService {
