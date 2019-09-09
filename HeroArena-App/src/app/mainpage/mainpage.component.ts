06bc7f386e19b2b912286d238bb9294d1995272c
Jim Yount <jimayount@gmail.com>
2019-09-09 01:44:23 -0400

    Almost Final Version

--- HeroArena-App/src/app/mainpage/mainpage.component.ts
+++ HeroArena-App/src/app/mainpage/mainpage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RosterDisplayComponent } from '../roster-display/roster-display.component';
 import { RosterService} from '../roster.service';
 import { PagestateService} from '../pagestate.service';
+import { AuthenticateService} from '../authenticate.service';
 
 @Component({
   selector: 'app-mainpage',
@@ -10,7 +11,7 @@ import { PagestateService} from '../pagestate.service';
 })
 export class MainpageComponent implements OnInit {
   
-  constructor(private rs: RosterService, private pss: PagestateService) { }
+  constructor(private rs: RosterService, private pss: PagestateService, private as: AuthenticateService) { }
 
   ngOnInit() {
     this.pss.setState(0);
@@ -32,5 +33,13 @@ export class MainpageComponent implements OnInit {
 
   showShop(){
     this.pss.setState(4);
+  }
+
+  showManageUsers(){
+    this.pss.setState(5);
+  }
+
+  showManageChars(){
+    this.pss.setState(6);
   }
 }
